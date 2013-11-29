70fb3dc3a8e9b993861530a9599a7a688b27ad68
HyunJune Kim <hyunjune.kim@samsung.com>
2013-11-29 18:51:49 +0900

    Add android surface.

--- platform/android/surface.rs
+++ platform/android/surface.rs
@@ -17,7 +17,20 @@ use opengles::gl2::NO_ERROR;
 use opengles::gl2;
 use std::cast;
 use std::libc::{c_char, c_int, c_uint, c_void};
+use std::libc::funcs::c95::stdlib::{malloc, free};
 use std::ptr;
+
+use egl::egl::NativeWindowType;
+use egl::egl::{EGLConfig, EGLint, EGLBoolean};
+use egl::egl::{EGL_NO_DISPLAY, EGL_DEFAULT_DISPLAY, EGL_NO_CONTEXT, EGL_CONTEXT_CLIENT_VERSION, EGL_NONE};
+use egl::egl::{EGLDisplay, EGLSurface, EGLContext, EGL_TRUE, EGL_FALSE, EGL_WIDTH, EGL_HEIGHT};
+use egl::egl::{EGL_SURFACE_TYPE, EGL_WINDOW_BIT, EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+                EGL_NONE, EGL_NO_SURFACE, EGL_BUFFER_SIZE, EGL_DEPTH_SIZE, EGL_DRAW};
+use egl::egl::{GetDisplay, Initialize, CreateContext, MakeCurrent};
+use egl::egl::{QuerySurface, ChooseConfig, GetError};
+use egl::egl::{CreateWindowSurface, SwapBuffers, DestroyContext, GetCurrentDisplay, GetCurrentSurface, GetCurrentContext};
+use egl::egl::{EGL_RED_SIZE, EGL_BLUE_SIZE, EGL_GREEN_SIZE};
+
 
 /// The display and visual info. This is needed in order to upload on the painting side. This holds
 /// a *strong* reference to the display and will close it when done.
@@ -25,7 +38,7 @@ use std::ptr;
 /// FIXME(pcwalton): Mark nonsendable and noncopyable.
 pub struct NativePaintingGraphicsContext{
     //To be implemented
-    dummy: bool,
+    display:EGLDisplay
 }
 
 impl NativePaintingGraphicsContext {
@@ -34,7 +47,7 @@ impl NativePaintingGraphicsContext {
         //To be implemented
         unsafe {
             NativePaintingGraphicsContext {
-                dummy : false,
+                display : GetCurrentDisplay(),
             }
         }
     }
@@ -44,6 +57,7 @@ impl Drop for NativePaintingGraphicsContext {
     #[fixed_stack_segment]
     fn drop(&mut self) {
         //To be implemented
+        let display: EGLDisplay = GetDisplay(EGL_DEFAULT_DISPLAY as *c_void);
     }
 }
 
@@ -58,17 +72,24 @@ impl Drop for NativePaintingGraphicsContext {
 /// FIXME(pcwalton): Mark nonsendable.
 pub struct NativeCompositingGraphicsContext {
     //To be implemented
-    dummy : bool,
+    display : EGLDisplay,
+    surface : EGLSurface,
+    context : EGLContext,
 }
 
 impl NativeCompositingGraphicsContext {
     pub fn new() -> NativeCompositingGraphicsContext {
         //To be implemented
+
         NativeCompositingGraphicsContext {
-            dummy : true,
+            display : GetCurrentDisplay(),
+            surface : GetCurrentSurface(EGL_DRAW as i32),
+            context : GetCurrentContext(),
         }
     }
 }
+
+
 
 pub struct NativeGraphicsMetadata {
     dummy: bool,
@@ -82,6 +103,7 @@ pub enum NativeSurfaceTransientData {
 pub struct NativeSurface {
     //To be implemented
     /// Whether this pixmap will leak if the destructor runs. This is for debugging purposes.
+    pixmap: *c_void,
     will_leak: bool,
 }
 
@@ -98,6 +120,7 @@ impl NativeSurface {
     #[fixed_stack_segment]
     pub fn from_eglsurface(surface: /*to be changed to egl surface type*/*c_void) -> NativeSurface {
         NativeSurface {
+            pixmap: surface,
             will_leak: true,
         }
     }
@@ -108,9 +131,14 @@ impl NativeSurfaceMethods for NativeSurface {
     fn new(native_context: &NativePaintingGraphicsContext, size: Size2D<i32>, stride: i32)
            -> NativeSurface {
                //To be implemented
+        let mut len = size.width * size.height * (stride/size.width);
+        unsafe {
+             let mut data = malloc(len as u32);
                NativeSurface {
+                   pixmap: cast::transmute(data),
                    will_leak: false,
                }
+        }
     }
 
     /// This may only be called on the compositor side.
@@ -120,6 +148,8 @@ impl NativeSurfaceMethods for NativeSurface {
                        texture: &Texture,
                        _size: Size2D<int>) {
         //To be implemented
+        // texture bind
+        // glTexture2D function
     }
 
     /// This may only be called on the painting side.
@@ -130,13 +160,14 @@ impl NativeSurfaceMethods for NativeSurface {
 
     fn get_id(&self) -> int {
         //To be implemented
-        1
+        self.pixmap as int
     }
 
 #[fixed_stack_segment]
     fn destroy(&mut self, graphics_context: &NativePaintingGraphicsContext) {
         unsafe {
             //To be implemented
+            free(self.pixmap);
             self.mark_wont_leak()
         }
     }
